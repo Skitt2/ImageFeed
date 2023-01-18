8a481e432d223d39e21fea79743cc98cb79ca762
Skitt2 <provaid@yandex.ru>
2023-01-18 08:35:51 +0300

    Добавлен экран профиля по макету

---
+++ ImageFeed/Profile/ProfileViewController.swift
@@ -0,0 +1,11 @@
+import UIKit
+
+final class ProfileViewController: UIViewController {
+    @IBOutlet var profilePhoto: UIImageView!
+    @IBOutlet var arrowButton: UIButton!
+    @IBOutlet var profileName: UILabel!
+    @IBOutlet var profileID: UILabel!
+    @IBOutlet var profileMessage: UILabel!
+    
+    
+}
