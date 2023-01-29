cbdabd1137326149029204448da1a3c221319be8
Skitt2 <provaid@yandex.ru>
2023-01-29 14:06:49 +0300

    добавлены переходы межу экранами

--- ImageFeed/Profile/ProfileViewController.swift
+++ ImageFeed/Profile/ProfileViewController.swift
@@ -2,10 +2,12 @@ import UIKit
 
 final class ProfileViewController: UIViewController {
     @IBOutlet var profilePhoto: UIImageView!
-    @IBOutlet var arrowButton: UIButton!
     @IBOutlet var profileName: UILabel!
     @IBOutlet var profileID: UILabel!
+    @IBOutlet var arrowButton: UIButton!
     @IBOutlet var profileMessage: UILabel!
     
+    @IBAction func arrowButton(_ sender: Any) {
+    }
     
 }
