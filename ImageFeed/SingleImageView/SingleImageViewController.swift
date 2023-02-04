d26fd7662da25796d89bc883a894c2c3e10dfe89
Skitt2 <provaid@yandex.ru>
2023-02-04 14:54:40 +0300

    исправлены констрейты у вида профиля

--- ImageFeed/SingleImageView/SingleImageViewController.swift
+++ ImageFeed/SingleImageView/SingleImageViewController.swift
@@ -1,6 +1,7 @@
 import UIKit
 
 final class SingleImageViewController: UIViewController {
+    
     var image: UIImage! {
         didSet {
             guard isViewLoaded else {return}
@@ -9,6 +10,12 @@ final class SingleImageViewController: UIViewController {
     }
     
     @IBOutlet var imageView: UIImageView!
+    
+    @IBAction private func didTapBackButton() {
+        dismiss(animated: true, completion: nil)
+    }
+    
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
