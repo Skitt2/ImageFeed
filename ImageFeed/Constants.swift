3efe6b4802ded1f92f254ae2e78a768d6f6e5c3e
Skitt2 <provaid@yandex.ru>
2023-06-04 14:15:15 +0300

    добавлены AuthView и WebView

---
+++ ImageFeed/Constants.swift
@@ -0,0 +1,7 @@
+import Foundation
+
+let AccessKey = "oVUKw0aXW9M4lFfsi4Jqe9ZaY_7Bcdruha50CRwPNzY"
+let SecretKey = "_VJ0W62xDhxbqNM3uWLRStlM-hz3IsoQpVWh-5zqVoE"
+let RedirectURI = "urn:ietf:wg:oauth:2.0:oob"
+let AccessScope = "public+read_user+write_likes"
+let DefaultBaseURL = URL(string: "https://api.unsplash.com")!
